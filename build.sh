8a90e3932b18d27ea8126e02d245bfa620178d3f
Oguz Mutlu <oguzmutlu2001@gmail.com>
2025-06-20 23:32:07 +0200

    build script

--- build.sh
+++ build.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
 
-if [ "$1" != "" ]
-then
-  COMPILER="$1"
-fi
+# if [ "$1" != "" ]
+# then
+#   COMPILER="$1"
+# fi
 
-git submodule update --init --recursive
+#git submodule update --init --recursive
 cd lib/cbmc
-git checkout $CBMC_VERSION
-if grep '^MINISAT2' src/config.inc > /dev/null
-then
-  make -C src minisat2-download > /dev/null
-elif grep '^GLUCOSE' src/config.inc
-then
-  make -C src glucose-download
-else
-  echo "SAT solver not supported"
-  exit 1
-fi
-if [ "$COMPILER" != "" ]
-then
-  make -C src CXX=$COMPILER $2
-else
-  make -C src $2
-fi
+git checkout 2ls5.61
+./build_with_cadical.sh
+# git checkout $CBMC_VERSION
+# if grep '^MINISAT2' src/config.inc > /dev/null
+# then
+#   make -C src minisat2-download > /dev/null
+# elif grep '^GLUCOSE' src/config.inc
+# then
+#   make -C src glucose-download
+# else
+#   echo "SAT solver not supported"
+#   exit 1
+# fi
+# if [ "$COMPILER" != "" ]
+# then
+#   make -C src CXX=$COMPILER $2
+# else
+#   make -C src $2
+# fi
 cd ../..
-
-if [ "$COMPILER" != "" ]
-then
-  make -C src CXX=$COMPILER $2
-else
-  make -C src $2
-fi
+make -C src LIBS="$PWD/lib/cbmc/cadical/build/libcadical.a" IPASIR=$PWD/lib/cbmc/cadical/src -j16
+# if [ "$COMPILER" != "" ]
+# then
+#   make -C src CXX=$COMPILER $2
+# else
+#   make -C src $2
+# fi
 
 echo "The executable is src/2ls/2ls"
