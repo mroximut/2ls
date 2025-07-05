f0c4f0d13e2691133375250c60a498d633087bf0
Oguz Mutlu <oguzmutlu2001@gmail.com>
2025-07-06 00:03:21 +0200

    log statistics

--- build.sh
+++ build.sh
@@ -1,38 +1,15 @@
 #!/bin/bash
 
-# if [ "$1" != "" ]
-# then
-#   COMPILER="$1"
-# fi
-
-#git submodule update --init --recursive
+if [ ! -d "lib/cbmc" ]; then
+  mkdir -p lib/cbmc
+  cd lib/cbmc
+  git clone git@github.com:mroximut/cbmc.git --branch 2ls5.61
+  cd ../..
+fi
 cd lib/cbmc
-git checkout 2ls5.61
-./build_with_cadical.sh
-# git checkout $CBMC_VERSION
-# if grep '^MINISAT2' src/config.inc > /dev/null
-# then
-#   make -C src minisat2-download > /dev/null
-# elif grep '^GLUCOSE' src/config.inc
-# then
-#   make -C src glucose-download
-# else
-#   echo "SAT solver not supported"
-#   exit 1
-# fi
-# if [ "$COMPILER" != "" ]
-# then
-#   make -C src CXX=$COMPILER $2
-# else
-#   make -C src $2
-# fi
+bash ./build_with_cadical.sh
+
 cd ../..
 make -C src LIBS="$PWD/lib/cbmc/cadical/build/libcadical.a" IPASIR=$PWD/lib/cbmc/cadical/src -j16
-# if [ "$COMPILER" != "" ]
-# then
-#   make -C src CXX=$COMPILER $2
-# else
-#   make -C src $2
-# fi
 
 echo "The executable is src/2ls/2ls"
--- src/2ls/2ls_main.cpp
+++ src/2ls/2ls_main.cpp
@@ -10,8 +10,11 @@ Author: Daniel Kroening, Peter Schrammel
 /// 2LS Main Module
 
 #include <util/unicode.h>
+#include <iostream>
+#include <chrono>
 
 #include "2ls_parse_options.h"
+#include "solvers/sat/satcheck_ipasir.h"
 
 #ifdef _MSC_VER
 
@@ -26,8 +29,22 @@ int wmain(int argc, const wchar_t **argv_wide)
 
 int main(int argc, const char **argv)
 {
+  auto start_time = std::chrono::steady_clock::now();
+  for(int i = 0; i < argc; ++i)
+  {
+    std::cout << "argv[" << i << "]: " << argv[i] << std::endl;
+  }
+
   twols_parse_optionst parse_options(argc, argv);
-  return parse_options.main();
+  int res = parse_options.main();
+
+  auto end_time = std::chrono::steady_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+  std::cout << "t PROCESSING_TIME: " << duration.count() / 1000.0f << std::endl;
+  std::cout << "t FINAL SAT_TIME: " << satcheck_ipasirt::sat_time << std::endl;
+  std::cout << "t FINAL SAT_CALLS: " << satcheck_ipasirt::sat_calls << std::endl;
+  std::cout << "s FINAL EC=" << res << std::endl;
+  return res;
 }
 
 #endif
