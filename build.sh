4f70647976d2001dc2a6f492ee74b181f5cc8ec2
Oguz Mutlu <oguzmutlu2001@gmail.com>
2025-07-06 00:19:01 +0200

    adjust build script

--- build.sh
+++ build.sh
@@ -1,39 +1,18 @@
 #!/bin/bash
 
-# if [ "$1" != "" ]
-# then
-#   COMPILER="$1"
-# fi
+if [ ! -d "lib/cbmc" ]; then
+  mkdir -p lib
+  cd lib
+  git clone git@github.com:mroximut/cbmc.git --branch 2ls5.61_connector
+  cd ..
+fi
 
-#git submodule update --init --recursive
+
 cd lib/cbmc
-#git checkout 2ls5.61_app_for_mallob
 bash ./build_with_mallob.sh
-# git checkout $CBMC_VERSION
-# if grep '^MINISAT2' src/config.inc > /dev/null
-# then
-#   make -C src minisat2-download > /dev/null
-# elif grep '^GLUCOSE' src/config.inc
-# then
-#   make -C src glucose-download
-# else
-#   echo "SAT solver not supported"
-#   exit 1
-# fi
-# if [ "$COMPILER" != "" ]
-# then
-#   make -C src CXX=$COMPILER $2
-# else
-#   make -C src $2
-# fi
+
 cd ../..
 rm ./src/2ls/lib2ls.a ./src/solver/solver.a
 bash ./lib/cbmc/build_with_mallob.sh
-# if [ "$COMPILER" != "" ]
-# then
-#   make -C src CXX=$COMPILER $2
-# else
-#   make -C src $2
-# fi
 
 echo "The executable is src/2ls/2ls"
