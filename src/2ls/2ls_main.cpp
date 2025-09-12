c9cc11bddac3af358ffc935faca1254181191833
Oguz Mutlu <oguzmutlu2001@gmail.com>
2025-09-12 18:41:16 +0200

    add dump-cnf option

--- src/2ls/2ls_main.cpp
+++ src/2ls/2ls_main.cpp
@@ -12,6 +12,7 @@ Author: Daniel Kroening, Peter Schrammel
 #include <util/unicode.h>
 #include <iostream>
 #include <chrono>
+#include <fstream>
 
 #include "2ls_parse_options.h"
 #include "solvers/sat/satcheck_ipasir.h"
@@ -27,16 +28,38 @@ int wmain(int argc, const wchar_t **argv_wide)
 
 #else
 
+// Global CNF file that can be accessed by satcheck_ipasir
+std::ofstream global_cnf_file;
+std::string cnf_filename = "";
+int cnf_file_position = 0;
+
 int main(int argc, const char **argv)
 {
   auto start_time = std::chrono::steady_clock::now();
   for(int i = 0; i < argc; ++i)
   {
     std::cout << "argv[" << i << "]: " << argv[i] << std::endl;
+    if (cnf_file_position > 0 && i >= cnf_file_position + 2) {
+      argv[i - 2] = argv[i];
+    }
+    if (argv[i] == std::string("--dump-cnf") && i + 1 < argc) {
+      cnf_filename = argv[i + 1];
+      cnf_file_position = i;
+    }
   }
+
+  if (cnf_file_position > 0) {
+    argc -= 2;
+    global_cnf_file.open(cnf_filename);
+  }
+
 
   twols_parse_optionst parse_options(argc, argv);
   int res = parse_options.main();
+
+  if (cnf_file_position > 0) {
+    global_cnf_file.close();
+  }
 
   auto end_time = std::chrono::steady_clock::now();
   auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
